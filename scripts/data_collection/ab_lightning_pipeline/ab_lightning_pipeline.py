7a6a21720fbc7a96d623512ec6b97c0a5dbca673
J. Romero <JRomero.yyc@gmail.com>
2025-03-01 19:35:31 -0700

    Correctly concatinate lightning dataframes
    - sorting based on 'date_group', lat, long

--- scripts/data_collection/ab_lightning_pipeline/ab_lightning_pipeline.py
+++ scripts/data_collection/ab_lightning_pipeline/ab_lightning_pipeline.py
@@ -26,30 +26,15 @@ class AbLightningPipeline:
                     except Exception as e:
                         logger.error(f"Failed to read {file_path}: {e}")
                         
-                      
-
-    # # Collect all csv files in the subdirectories, as dataframes
-    # for dir in self.subdirs_list:
-    #   print(dir)
-    #   for file in os.scandir(dir):
-    #     print(str(file))
-    #     if str(file)[-4:] == '.csv':
-    #       name = str(file)[:-4]
-    #       df = pd.read_csv(os.path.join(dir, file))
-    #       self.raw_dfs.append({name: df})
-    
     self.lightning_df = self.raw_dfs[0]
               
     for df in self.raw_dfs[1:]:
       if self.lightning_df is None:
         self.lightning_df = pd.DataFrame()
-      # Merge all dataframes into one
-      
-      pd.merge(self.lightning_df, df, on='date_group')
-      logger.info(f"Merged dataframe: {df} of length: {len(df)}, to lightning_df, of length: {len(self.lightning_df)}")
+      # Concatenate all dataframes into one
+      self.lightning_df = pd.concat([self.lightning_df, df]).sort_values(['date_group', 'latitude', 'longitude'])
+      logger.info(f"Merged dataframe of length: {len(df)}, to lightning_df, of length: {len(self.lightning_df)}")
 
-    # logger.info(f"Lightning dataframe created with length: {len(self.lightning_df)}")
-    # print(self.lightning_df.head())
       
   def get_ltng_df(self):
     return self.lightning_df
--- scripts/data_collection/test_abltng_pipeline.py
+++ scripts/data_collection/test_abltng_pipeline.py
@@ -6,5 +6,6 @@ data_dir = "scripts/data_collection/static_datasets/ablightning_historical"
 ablp = AbLightningPipeline(data_dir)
 
 ldf = ablp.get_ltng_df()
+# ldf.to_csv("scripts/data_collection/static_datasets/ablightning_historical/ab_lightning_data.csv", index=False)
 print(ldf.head())
 
