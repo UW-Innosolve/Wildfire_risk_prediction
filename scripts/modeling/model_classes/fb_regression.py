7849fc4623e531dea70e9f7ecf8e5fb98de10db1
J. Romero <JRomero.yyc@gmail.com>
2025-03-09 22:53:10 -0600

    Update fb_regression.py
    Wrote regression class
    includes methods for
    - linear
    - logistic
    - ridge
    - polynomial

--- scripts/modeling/model_classes/fb_regression.py
+++ scripts/modeling/model_classes/fb_regression.py
@@ -1,29 +1,47 @@
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression, RidgeCV
+from sklearn.preprocessing import PolynomialFeatures
+from model_evaluation.model_metrics import BaseModel
+import numpy as np
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 ## Inluding linear, polynomial, and logisitc regression modeling.
 ## consider other models.
-
 class FbRegressionModel(BaseModel):
-  def __init__(self, input_df):
-    self.x_data = input_df
-    
-    ## init hyperparams for lin:
-    
-    ## init hyperparams for poly:
-
-    ## init hyperparams for log:
-
+  def __init__(self):
+    logger.info("Initializing FbRegressionModel")
 
   def config_lin_reg(self):
-    pass
+    self.lin_reg_model = LinearRegression(**self.lin_reg_params)
   
-  def config_poly_reg(self):
-    pass
-  
-  def config_log_reg(self):
-    pass
+  def config_poly_reg(self, degree=3):
+    self.poly_reg_params = {'degree': degree}
+    self.poly_reg_model = PolynomialFeatures(**self.poly_reg_params)
+    
+    
+  def config_log_reg(self, c=1.0, solver='liblinear', class_weight='balanced', max_iter=1000, tol=1e-4):
+    self.log_reg_params = {'C': c, 'solver': solver, 'class_weight': class_weight, 'max_iter': max_iter, 'tol': tol}
+    self.log_reg_model = LogisticRegression(**self.log_reg_params)
+    
+  def config_ridge_reg(self, solver='auto', tol=1e-4):
+    alphas= np.array([1.e-06, 1.e-05, 1.e-04, 1.e-03, 1.e-02, 1.e-01, 1.e+00, 1.e+01,
+      1.e+02, 1.e+03, 1.e+04, 1.e+05, 1.e+06])
+    self.ridge_reg_params = {'alphas': alphas, 'solver': solver, 'tol': tol}
+    self.ridge_reg_model = RidgeCV(**self.ridge_reg_params) 
+    # RidgeCV automatically performs cross-validation (on the training data) to find the optimal alpha value.
   
   # NOTE: All model types should have default setups.
   def train(self, x_train, y_train,
-            lin_reg=True, poly_reg=True, log_reg=True):
-    pass
+            lin_reg=True, poly_reg=True, log_reg=True, ridge_reg=True):
+    if lin_reg:
+      self.lin_reg_model.fit(x_train, y_train)
+    if poly_reg:
+      self.poly_reg_model.fit(x_train, y_train)
+    if log_reg:
+      self.log_reg_model.fit(x_train, y_train)
+    if ridge_reg:
+      self.ridge_reg_model.fit(x_train, y_train)
