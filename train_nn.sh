eb7d4782b5c731024b77dcf52605fe5c28374cb5
Teodora Vujovic <teo.vujovic@gmail.com>
2025-03-27 03:50:23 -0400

    updating shell script for training lstm

---
+++ train_nn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=def-nassersa-ab
+#SBATCH --mail-user=teo.vujovic@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=50G
+#SBATCH --time=0-01:00
+
+cd /home/tvujovic/scratch/
+source dataprocess/bin/activate
+cd ./firebird/Wildfire_risk_prediction/scripts/modeling/
+
+python main.py
