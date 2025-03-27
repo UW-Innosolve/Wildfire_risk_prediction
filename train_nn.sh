44a50193b851cadab186616bf3e652210f2b42fd
Teodora Vujovic <teo.vujovic@gmail.com>
2025-03-27 03:51:06 -0400

    updating shell script for training lstm

--- train_nn.sh
+++ train_nn.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-nassersa-ab
 #SBATCH --mail-user=teo.vujovic@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH --mem-per-cpu=50G
+#SBATCH --mem-per-cpu=100G
 #SBATCH --time=0-01:00
 
 cd /home/tvujovic/scratch/
