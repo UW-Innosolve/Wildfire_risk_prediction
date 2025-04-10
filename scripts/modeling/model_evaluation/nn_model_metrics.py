dc795466b008a067a05c8ec478ae89d6f6c0e018
Teodora Vujovic <teo.vujovic@gmail.com>
2025-04-10 16:27:51 -0400

    updated metrics, excluded roc_auc_score for now (troubleshoot later), duplicated function to return dictionary vs individual metrics

--- scripts/modeling/model_evaluation/nn_model_metrics.py
+++ scripts/modeling/model_evaluation/nn_model_metrics.py
@@ -1,6 +1,5 @@
-
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score,f1_score, roc_auc_score
 
 
 # class BaseModel:
@@ -8,7 +7,8 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 #     Base model class that provides a generic evaluate method.
 #     All model classes will inherit from this to avoid repeating evaluation code.
 #     """
-def evaluate(predictions, y_test):
+# TODO: troubleshoot roc_auc_score
+def evaluate(predictions, targets, flat_shape, threshold_value=0.515):
     """
     Evaluate the model using standard metrics:
       - Accuracy: Overall correctness.
@@ -28,12 +28,53 @@ def evaluate(predictions, y_test):
     #     # If predict_proba isn't available, use decision_function as a fallback.
     #     probs = model.decision_function(X_test)
 
+    # flatten arrays
+    predictions_flat = predictions.detach().numpy().reshape(flat_shape)
+    targets_flat = targets.detach().numpy().reshape(flat_shape)
+
+    # threshold predictions to give 1 and 0
+    thresholded_predictions_flat = (predictions_flat > threshold_value).astype(int)
+
     # Compute metrics using scikit-learn's functions.
     metrics = {
-        "accuracy": accuracy_score(y_test, predictions),
-        "precision": precision_score(y_test, predictions),
-        "recall": recall_score(y_test, predictions),
-        "f1_score": f1_score(y_test, predictions),
-        # "roc_auc": roc_auc_score(y_test, probs)
+        "accuracy": accuracy_score(targets_flat.astype(int), thresholded_predictions_flat),
+        "precision": precision_score(targets_flat.astype(int), thresholded_predictions_flat),
+        "recall": recall_score(targets_flat.astype(int), thresholded_predictions_flat),
+        "f1": f1_score(targets_flat.astype(int), thresholded_predictions_flat),
+        # "roc_auc": roc_auc_score(targets_flat, predictions_flat)
     }
     return metrics
+
+def evaluate_individuals(predictions, targets, flat_shape, threshold_value=0.515):
+    """
+    Evaluate the model using standard metrics:
+      - Accuracy: Overall correctness.
+      - Precision: Ratio of true positive predictions to total positive predictions.
+      - Recall: Ratio of true positive predictions to actual positives.
+      - F1 Score: Harmonic mean of precision and recall.
+      - ROC-AUC: Measure of separability.
+
+    Returns:
+        accuracy, precision, recall, f1_score
+    """
+    # Predict class labels using the model's predict method.
+    # try:
+    #     # Some models provide predict_proba for probability estimates.
+    #     probs = model.predict_proba(X_test)[:, 1]
+    # except AttributeError:
+    #     # If predict_proba isn't available, use decision_function as a fallback.
+    #     probs = model.decision_function(X_test)
+
+    # flatten arrays
+    predictions_flat = predictions.detach().numpy().reshape(flat_shape)
+    targets_flat = targets.detach().numpy().reshape(flat_shape)
+
+    # threshold predictions to give 1 and 0
+    thresholded_predictions_flat = (predictions_flat > threshold_value).astype(int)
+
+    accuracy = accuracy_score(targets_flat, thresholded_predictions_flat)
+    precision = precision_score(targets_flat, thresholded_predictions_flat)
+    recall = recall_score(targets_flat, thresholded_predictions_flat)
+    f1 = f1_score(targets_flat, thresholded_predictions_flat)
+
+    return accuracy, precision, recall, f1
